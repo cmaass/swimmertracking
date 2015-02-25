73db4badb4a86d6f5ed379df445e84ee73cec615
cmaass <cmaass@users.noreply.github.com>
2015-02-25 10:24:28 +0100

    wrong file

--- test.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/windows/D/goettingen/python/swimmertracking')
-import readtraces as rt
-mov=rt.movie('/windows/D/datagoe/140507/12.5wtpcTTAB_cut.mp4')
-mov.loadTrajectories()
-mov.plotMovie('/home/corinna/test.avi', lenlim=50,frate=5)
